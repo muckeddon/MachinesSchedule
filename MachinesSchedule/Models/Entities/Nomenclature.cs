e68a3da4550ca6a67c412f7d9a46a715fb690f30
Александр <HellaHoesCity@gmail.com>
2021-08-29 10:38:12 +0700

    Создан класс Nomenclature для хранения и работы с номенклатурами обрабатываемого металла

---
+++ MachinesSchedule/Models/Entities/Nomenclature.cs
@@ -0,0 +1,10 @@
+﻿
+namespace MachinesSchedule.Models.Entities
+{
+    public class Nomenclature
+    {
+        public int Id { get; set; }
+        public int NomenclatureId { get; set; }
+        public string NomenclatureName { get; set; }
+    }
+}
