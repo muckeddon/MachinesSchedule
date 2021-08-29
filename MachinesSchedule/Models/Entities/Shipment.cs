c0c9f8ac156151b5292f5e67b831ea61774ddadd
Александр <HellaHoesCity@gmail.com>
2021-08-29 10:39:12 +0700

    Создан класс Shipment для хранения и работы с партиями металлов.

---
+++ MachinesSchedule/Models/Entities/Shipment.cs
@@ -0,0 +1,10 @@
+﻿
+namespace MachinesSchedule.Models.Entities
+{
+    public class Shipment
+    {
+        public int Id { get; set; }
+        public int ShipmentId { get; set; }
+        public int NomenclatureId { get; set; }
+    }
+}
