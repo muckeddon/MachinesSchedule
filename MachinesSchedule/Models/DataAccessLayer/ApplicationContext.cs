98b161c56c15053f977eb2670339057d686127d2
Александр <HellaHoesCity@gmail.com>
2021-08-29 10:42:14 +0700

    Изменен класс ApplicationContext, добавлены свойства для бд.

--- MachinesSchedule/Models/DataAccessLayer/ApplicationContext.cs
+++ MachinesSchedule/Models/DataAccessLayer/ApplicationContext.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using MachinesSchedule.Models.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace MachinesSchedule.Models.DataAccessLayer
 {
     public class ApplicationContext : DbContext
     {
+        #region Properties
+        public DbSet<MachineTool> MachineTools { get; set; }
+        public DbSet<Nomenclature> Nomenclature { get; set; }
+        public DbSet<Shipment> Shipment { get; set; }
+        public DbSet<Time> Time { get; set; }
+        #endregion
         public ApplicationContext(DbContextOptions<ApplicationContext> options)
             : base(options)
         {
