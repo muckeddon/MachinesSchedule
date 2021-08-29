b77ec5fabbaa930ad7d72fd108e55cfcb39cfdbd
Александр <HellaHoesCity@gmail.com>
2021-08-29 10:44:41 +0700

    Добавлен класс ImportExcelViewModel для передачи данных из вью в конструктор для импорта файлов эксель с входными данными.

---
+++ MachinesSchedule/ViewModels/ImportExcelViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace MachinesSchedule.ViewModels
+{
+    public class ImportExcelViewModel
+    {
+        #region Properties
+        [Required]
+        [Display(Name = "Выбор документа")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Загрузить файл")]
+        public IFormFile Document { get; set; }
+        #endregion
+    }
+}
