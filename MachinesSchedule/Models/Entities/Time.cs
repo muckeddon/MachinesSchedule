de4027b7984502430cc85ce4282862310d4ab1f4
Александр <HellaHoesCity@gmail.com>
2021-08-29 10:40:19 +0700

    Создан класс Time для хранения и работы с временем работы каждой единицы оборудования по времени

---
+++ MachinesSchedule/Models/Entities/Time.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MachinesSchedule.Models.Entities
+{
+    public class Time
+    {
+        public int Id { get; set; }
+        public int MachineToolId { get; set; }
+        public int NomenclatureId { get; set; }
+        public int OperationTime { get; set; }
+    }
+}
